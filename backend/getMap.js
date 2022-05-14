fda70240dcdd063c747b10d42bb82582e1a61fbb
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2022-05-14 11:34:08 -0300

    ajustes na resolucao do mapa cache e teste isolado com 1 dado

--- backend/getMap.js
+++ backend/getMap.js
@@ -4,20 +4,34 @@ export function getMapData(mapName) {
       var sampleSVG = d3
         .select("#left-mapcard")
         .append("svg")
-        .attr("width", 342)
-        .attr("height", 460);
+        .attr("width", 450)
+        .attr("height", 450);
 
+      //for (let index = 0; index < data.length; index++) {
       sampleSVG
         .append("circle")
         .style("stroke", "gray")
-        .style("fill", "white")
-        .attr("r", 40)
-        .attr("cx", function (d, i) {
-          return 50 + i * 80;
-        })
-        .attr("cy", 120);
+        .style("fill", "red")
+        .attr("r", 3)
+        .attr("cx", CalculatePointToResolutionX(-156.3252))
+        .attr("cy", CalculatePointToResolutionY(196.0616));
+      //}
     });
   } else if (mapName == "de_mirage") {
   } else {
   }
+}
+
+function CalculatePointToResolutionX(x) {
+  x += 2031;
+  x = Math.floor((x / 3752) * 450);
+  console.log(x);
+  return x;
+}
+function CalculatePointToResolutionY(y) {
+  y += 2240;
+  y = Math.floor((y / 3187) * 450);
+  y = y - 450;
+  console.log(y);
+  return y;
 }
